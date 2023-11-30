9ba77f11083ce8f94cc5c6de77979cd64f11803c
ShamanShine <lazun2016@gmail.com>
2023-12-01 00:56:41 +0200

    fix 1 code

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
 function slugify(title) {
-  const slugTitle = title.join('-').toLowerCase(); //Сначала обьеденяем элементы масива в стоку с разделителем "-", затем задаём нижний реестр
-  console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-  console.log(slugify('English for developer')); // "english-for-developer"
-  console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-  console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer
+  const slugTitle = title.split(' ').join('-').toLowerCase(); //Сначала используем метод для разденления строки на масив подстрок, затем обьеденяем элементы масива в стоку с разделителем "-", затем задаём нижний реестр
+  return slugTitle;
 }
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer
