7b14d52db4a4c8ddab0778c96bf9e396c676523f
ShamanShine <lazun2016@gmail.com>
2023-12-01 01:24:25 +0200

    fix code

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,10 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
-  if (newArray > maxLength) {
-    return maxLength.slice();
+  const newArray = firstArray.concat(secondArray); //сначала соединям два массива
+  if (newArray.length > maxLength) {
+    // прописываем условия
+    return newArray.slice(0, maxLength);
   } else {
-    return newArray.slice;
+    return newArray.slice();
   }
 }
 
